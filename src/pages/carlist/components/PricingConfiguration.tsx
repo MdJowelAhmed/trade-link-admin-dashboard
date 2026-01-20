965f327a4a7dc493de591dc48d8c933f0a1af4c9
MdJowelAhmed <mdjowelahmed924@gmail.com>
2026-01-20 12:17:20 +0600

    style: car details modal update

--- src/pages/carlist/components/PricingConfiguration.tsx
+++ src/pages/carlist/components/PricingConfiguration.tsx
@@ -1,6 +1,6 @@
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {  CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/utils/cn";
 
