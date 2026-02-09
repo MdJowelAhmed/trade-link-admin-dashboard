12aecaa1b6c0e9196dceb5ba7ce0a481e13daa26
MdJowelAhmed <mdjowelahmed924@gmail.com>
2026-02-09 11:20:31 +0600

    add api

--- src/redux/api/bonusManageApi.ts
+++ src/redux/api/bonusManageApi.ts
@@ -8,14 +8,33 @@ const bonusManageApi = baseApi.injectEndpoints({
                 if (args?.searchTerm) queryParams.append('searchTerm', args.searchTerm)
                 if (args?.page) queryParams.append('page', args.page.toString())
                 if (args?.limit) queryParams.append('limit', args.limit.toString())
+                const queryString = queryParams.toString()
                 return {
-                    url: '/admin/settings/bonus-management',
+                    url: `/admin/settings/bonus-management${queryString ? `?${queryString}` : ''}`,
                     method: 'GET',
                 }
             },
             providesTags: ['BonusManagement'],
         }),
-    }),
+        updateBonusManagementAmount: builder.mutation<BonusManagementResponse, BonusManagement>({
+            query: (bonusManagement) => ({
+                url: '/admin/settings/bonus-management',
+                method: 'PATCH',
+                body: bonusManagement,
+            }),
+            invalidatesTags: ['BonusManagement'],
+        }),
+        UpdateBonusManagementStatus: builder.mutation<BonusManagementResponse, BonusManagement>({
+            query: (bonusManagement) => ({
+                url: '/admin/settings/bonus-management',
+                method: 'PATCH',
+                body: bonusManagement,
+            }),
+            invalidatesTags: ['BonusManagement'],
+        }),
+
+    }), 
+
 })
 
-export const { useGetBonusManagementQuery } = bonusManageApi
+export const { useGetBonusManagementQuery, useUpdateBonusManagementAmountMutation, useUpdateBonusManagementStatusMutation } = bonusManageApi
